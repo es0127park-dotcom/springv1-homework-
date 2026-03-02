4b845bcaddd6511686af75deb3eaafcd059f2cee
parkcoding <es0127.park@gmail.com>
2026-03-02 22:41:12 +0900

    2-1. 프로젝트 초기 화면 구성과 구조 이해 완료

---
+++ src/main/java/com/example/springv1/user/UserController.java
@@ -0,0 +1,18 @@
+package com.example.springv1.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/join-form")
+    public String joinForm() {
+        return "user/join-form";
+    }
+
+    @GetMapping("/login-form")
+    public String loginForm() {
+        return "user/login-form";
+    }
+}
